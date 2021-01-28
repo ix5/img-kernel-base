a8480b3bea20adde9f87acf3fe112ff98487040f
ix5 <ix5@users.noreply.github.com>
2021-01-28 21:33:48 +0100

    local.sh: Use vars, comment

--- local.sh
+++ local.sh
@@ -1,10 +1,18 @@
-cp -r ~/dev/android/workdir/prebuilts/clang/host/linux-x86/clang-r353983b .
-cp -r ~/dev/android/workdir/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/ gcc-arm-linux-androideabi-4.9
-cp -r ~/dev/android/workdir/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/ gcc-aarch64-linux-android-4.9
+ANDROID_WORKDIR=~/dev/android/workdir
 
-cp ~/dev/android/workdir/prebuilts/clang/host/linux-x86/README.md clang-README.md
+cp -r $ANDROID_WORKDIR/prebuilts/clang/host/linux-x86/clang-r353983b .
 
-cp ~/dev/android/workdir/out/soong/host/linux-x86/bin/mkbootimg .
-cp ~/dev/android/workdir/out/soong/host/linux-x86/bin/mkdtimg .
+# Remove cruft
+find clang-r349610 -type f -name "*.8svn" -exec rm {} \;
+rm -r clang-r349610/runtimes_ndk_cxx/
+# For finding latest clang version in out-of-tree script
+cp $ANDROID_WORKDIR/prebuilts/clang/host/linux-x86/README.md clang-README.md
 
-cp ~/dev/android/workdir/out/host/linux-x86/lib64/libc++.so libc++.so
+cp -r $ANDROID_WORKDIR/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/ gcc-arm-linux-androideabi-4.9
+cp -r $ANDROID_WORKDIR/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/ gcc-aarch64-linux-android-4.9
+
+cp $ANDROID_WORKDIR/out/soong/host/linux-x86/bin/mkbootimg .
+cp $ANDROID_WORKDIR/out/soong/host/linux-x86/bin/mkdtimg .
+
+# Needed for mkbootimg
+cp $ANDROID_WORKDIR/out/host/linux-x86/lib64/libc++.so libc++.so
